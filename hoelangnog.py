6630fa9898db409f6fe74f49bfac829e6f94f78e
Hans Kemme <hans.kemme@vrt.be>
2023-02-01 11:42:08 +0100

    added sneeuw

--- hoelangnog.py
+++ hoelangnog.py
@@ -6,3 +6,7 @@ today = datetime.date.today()
 future = datetime.date(2023,3,4)
 diff = future - today
 st.write(f"tis nog {diff.days} dagen tot skiverlof")
+
+sneeuw = st.button('Ik wil nu al sneeuw')
+if sneeuw:
+    st.snow()
